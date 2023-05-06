23d5aea9df8fb4ac6325c2db666fbcb59f6a8704
vanime <98891705+zizi2525@users.noreply.github.com>
2023-05-06 15:32:14 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  ignoreDuringBuilds: true,
+
   images: {
     domains: ['assets.codepen.io','imgs.yalla-group.com','online.alkoora.live'],
-}
+},
+   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
